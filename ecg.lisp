12550a3d6af63880a020b2f91022ff2d2d80d8e2
Philip Kaludercic <philipk@posteo.net>
2022-04-12 23:53:04 +0200

    Mention license in ecg.lisp header

--- ecg.lisp
+++ ecg.lisp
@@ -9,6 +9,23 @@
 ;; This script implements a simple web server, that provides a form to
 ;; generate a custom Emacs configuration.
 
+;;; License:
+
+;; Copyright (C) 2022  Philip Kaludercic
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Code:
 
 (declaim (optimize (speed 3) (space 3) (safety 0) (debug 1)))
